d07c39ff0688f435aa188064170cac8477c8bbd0
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:27:52 -0700

    build email-verification protocol with Nodemailer

---
+++ src/emailVerification.ts
@@ -0,0 +1,61 @@
+import jwt from "jsonwebtoken";
+import { TUser } from "../types/authTypes";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export const idFromUserObject = (user: TUser) => ({
+  id: user.id,
+});
+
+const BASE_URL = "http://localhost:3000";
+
+export const sendVerificationEmail = (user: TUser) => {
+  if (!process.env.EMAIL_JWT_SECRET)
+    return new Error("EMAIL_JWT_SECRET is missing from environment variables");
+
+  jwt.sign(
+    { user: idFromUserObject(user) },
+    process.env.EMAIL_JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+    (error, emailToken) => {
+      const url = `${BASE_URL}/confirmation/${emailToken}`;
+      if (!process.env.EMAIL_ADDRESS) {
+        return new Error("missing email from .env");
+      }
+      console.log(user.email, { email: process.env.EMAIL_ADDRESS });
+      transporter.sendMail(
+        {
+          from: "The Lesson Factory",
+          to: user.email,
+          subject: "Confirm your email!",
+          html: `
+          <p style="padding-left: 200px;">
+          Please confirm your email by clicking this link: 
+            <a href="${url}" style="background: rgb(58, 62, 148); padding: 10px 15px; text-decoration: none; color: white">Verify Email</a>
+          </p>
+            `,
+        },
+        (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log({ info: info.response });
+          }
+        }
+      );
+      console.log("email sent");
+    }
+  );
+};
---
+++ src/routers/emailVerification.router.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { prisma } from "../../prisma/db.setup";
+
+dotenv.config();
+
+const emailVerificationRouter = Router();
+
+emailVerificationRouter.get("/:token", async (req, res) => {
+  if (!process.env.EMAIL_JWT_SECRET) {
+    return res.status(404).send({ error: "no Email JWT Secret found" });
+  }
+  const decodedToken = jwt.verify(
+    req.params.token,
+    process.env.EMAIL_JWT_SECRET
+  );
+  if (typeof decodedToken !== "string" && "user" in decodedToken) {
+    const {
+      user: { id },
+    } = decodedToken;
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        emailVerified: true,
+      },
+    });
+    return res.redirect("http://localhost:5173");
+  } else {
+    return res.status(400).send({ error: "Invalid token" });
+  }
+});
+
+export { emailVerificationRouter };
