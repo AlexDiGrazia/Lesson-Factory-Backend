b52e7a352d79d9b9a22b91c1df311a4702a335db
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:27:02 -0700

    create authUtils.ts and build functions for authorization

---
+++ src/authUtils.ts
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { TUser } from "../types/authTypes";
+import dotenv from "dotenv";
+
+dotenv.config();
+const saltRounds = 11;
+
+export const encryptPassword = (password: string) =>
+  bcrypt.hash(password, saltRounds);
+
+export const createUnsecuredUserInformation = (user: TUser) => ({
+  id: user.id,
+  email: user.email,
+});
+
+export const createJWT = (user: TUser) => {
+  if (!process.env.USER_LOGIN_JWT_SECRET)
+    return new Error("JWT_SECRET is missing from environment variables");
+
+  return jwt.sign(
+    createUnsecuredUserInformation(user),
+    process.env.USER_LOGIN_JWT_SECRET
+  );
+};
