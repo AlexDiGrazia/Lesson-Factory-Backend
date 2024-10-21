99c7eec855d9ed78930a11800d46e23859e02ea6
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-20 23:51:20 -0700

    add authMiddleware to 'get all videos' endpoint

--- src/routers/videos.router.ts
+++ src/routers/videos.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { prisma } from "../../prisma/db.setup";
+import { authMiddleware } from "../authUtils";
 
 const videosRouter = Router();
 
-videosRouter.get("/", async (req, res) => {
+videosRouter.get("/", authMiddleware, async (req, res) => {
   const allVideos = await prisma.video.findMany();
   return res.status(200).send(allVideos);
 });
