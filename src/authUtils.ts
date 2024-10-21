81df071eb2009e6bd1463ee6c99428b20562ccee
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-21 13:37:21 -0700

    built auth middleware

--- src/authUtils.ts
+++ src/authUtils.ts
@@ -4,6 +4,16 @@ import { TUser } from "../types/authTypes";
 import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 
+// Extend the Request interface to include the user property
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: ReturnType<typeof createUnsecuredUserInformation>;
+  }
+}
+
+import { z } from "zod";
+import { prisma } from "../prisma/db.setup";
+
 dotenv.config();
 const saltRounds = 11;
 
@@ -27,11 +37,46 @@ export const createJWT = (user: TUser) => {
   );
 };
 
-export const authMiddleware = (
+const jwtSchema = z.object({
+  id: z.number(),
+  email: z.string(),
+  role: z.enum(["USER", "ADMIN"]),
+  emailVerified: z.boolean(),
+  iat: z.number(),
+});
+
+export const getDataFromJWT = (token?: string) => {
+  if (!token) return null;
+
+  try {
+    return jwtSchema.parse(
+      jwt.verify(token, process.env.USER_LOGIN_JWT_SECRET as string)
+    );
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log(req.headers.authorization?.split(""));
+  const [, token] = req.headers.authorization?.split(" ") || [];
+  const jwtData = getDataFromJWT(token);
+
+  if (!jwtData)
+    return res.status(401).json({ message: "Invalid authorization token" });
+
+  const userFromJWT = await prisma.user.findUnique({
+    where: {
+      email: jwtData.email,
+    },
+  });
+
+  if (!userFromJWT) return res.status(404).json({ message: "User not found" });
+
+  req.user = createUnsecuredUserInformation(userFromJWT);
   next();
 };
