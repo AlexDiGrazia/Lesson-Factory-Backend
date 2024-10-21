c6eba176be0717bfd26ffab5764c2ff65d004f98
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-21 13:38:12 -0700

    added authMiddleware to get firstVideo endpoint

--- src/routers/videos.router.ts
+++ src/routers/videos.router.ts
@@ -9,7 +9,7 @@ videosRouter.get("/", authMiddleware, async (req, res) => {
   return res.status(200).send(allVideos);
 });
 
-videosRouter.get("/firstVideo", async (req, res) => {
+videosRouter.get("/firstVideo", authMiddleware, async (req, res) => {
   const firstTableRow =
     await prisma.$queryRaw`SELECT * FROM "Video" LIMIT 1 OFFSET 4`;
   res.json(firstTableRow);
