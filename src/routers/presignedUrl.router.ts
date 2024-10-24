497c3e0cf4eeaece4eff4ad32f9648558e7b3d56
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-23 23:18:07 -0700

    changed signed URL experation life from 10 seconds to 60 seconds

--- src/routers/presignedUrl.router.ts
+++ src/routers/presignedUrl.router.ts
@@ -8,7 +8,7 @@ const { PRIVATE_KEY_TWO: privateKey, KEY_PAIR_ID_TWO: keyPairId } = process.env;
 const presignedUrlRouter = Router();
 
 presignedUrlRouter.post("/", async (req, res) => {
-  const dateLessThan = new Date(Date.now() + 1000 * 10).toString();
+  const dateLessThan = new Date(Date.now() + 1000 * 60).toString();
 
   if (privateKey === undefined || keyPairId === undefined) {
     return res.status(400).send("Environment variable not found");
