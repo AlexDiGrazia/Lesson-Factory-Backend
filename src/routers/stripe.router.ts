fb741b70c10fe6c120e58bf06747c0b6c0f24f57
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-26 21:43:05 -0700

    got Stripe checkout session working corectly. Form works now.

--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -9,31 +9,18 @@ const stripeRouter = Router();
 const DOMAIN = "http://localhost:5173";
 
 stripeRouter.post("/", async (req, res) => {
-  console.log("stripe ran ");
-  console.log({ secret_key: process.env.STRIPE_SECRET_KEY });
-
-  const product = await stripe.products.create({
-    name: "Membership",
-    id: "new_membership",
-    // default_price: "membership_price",
-  });
-
-  const price = await stripe.prices.create({
-    product: product.id,
-    unit_amount: 2000,
-    currency: "usd",
-    // id: "membership_price",
-  });
-
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
     line_items: [
       {
-        price: price.id,
+        // price: "price_1QDhp72NGxzi9IJzeoDYcTfy",
+        price: "price_1QDhnm2NGxzi9IJz4A1CC2zw",
         quantity: 1,
       },
     ],
-    mode: "subscription",
+    // mode: "subscription",
+    mode: "payment",
+    allow_promotion_codes: true,
     return_url: `${DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
     automatic_tax: { enabled: true },
   });
