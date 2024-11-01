3d7ed0cc3611c732f1dd560e4e42d670cc4f813a
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-31 18:21:58 -0700

    made endpoint able to dynamically handle different payment modes and payment amounts based on values passed via the req.body

--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -9,24 +9,42 @@ const stripeRouter = Router();
 const DOMAIN = "http://localhost:5173";
 
 stripeRouter.post("/", async (req, res) => {
+  const mode = req.body.mode;
+  const path = req.body.return_url;
+  const videoId = req.body.videoId;
+  const return_url =
+    mode === "subscription"
+      ? `${DOMAIN}/${path}`
+      : `${DOMAIN}/${path}/${videoId}`;
+
+  console.log(return_url);
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
     line_items: [
       {
-        // price: "price_1QDhp72NGxzi9IJzeoDYcTfy",  $20 recurring monthly
-        // price: "price_1QDhnm2NGxzi9IJz4A1CC2zw",  $20 one-off
-        price: "price_1QF2sz2NGxzi9IJzd5s4JSBk",
+        price: req.body.priceId,
         quantity: 1,
       },
     ],
-    // mode: "subscription",
-    mode: "payment",
+    mode: req.body.mode,
     allow_promotion_codes: true,
-    // return_url: `${DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
-    return_url: `${DOMAIN}/signup/payment/confirmation`,
+    return_url,
     automatic_tax: { enabled: true },
   });
   res.send({ clientSecret: session.client_secret });
 });
 
 export { stripeRouter };
+
+// price: "price_1QDhp72NGxzi9IJzeoDYcTfy",  $20 recurring monthly
+// price: "price_1QDhnm2NGxzi9IJz4A1CC2zw",  $20 one-off
+
+// mode: "subscription",
+// mode: "payment",
+
+// return_url: `${DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+// return_url: `${
+//   req.body.return_url === "subscription"
+//     ? `${DOMAIN}/signup/payment/confirmation`
+//     : `${DOMAIN}/signup/payment/video_purchase`
+// } `,
