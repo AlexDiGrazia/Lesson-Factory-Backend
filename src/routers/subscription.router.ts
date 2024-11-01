6ea6e0d5e3955f6f1c942d6d4b4eaba89f880640
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-31 18:16:38 -0700

    changed name of /subscription to /buy/subscription and added new endpoint /buy/single_video

--- src/routers/subscription.router.ts
+++ src/routers/subscription.router.ts
@@ -1,22 +1,64 @@
 import Router from "express";
 import { prisma } from "../../prisma/db.setup";
 import { createJWT } from "../authUtils";
+import { validateRequest } from "zod-express-middleware";
+import { z } from "zod";
 
 export const subscriptionRouter = Router();
 
-subscriptionRouter.post("/", async (req, res) => {
-  console.log("subscription updated");
-  const id = Number(req.body.id);
-  const updatedUser = await prisma.user.update({
-    where: {
-      id,
-    },
-    data: {
-      subscribed: true,
-    },
-  });
+subscriptionRouter.post(
+  "/subscription",
+  validateRequest({
+    body: z.object({
+      id: z.number(),
+    }),
+  }),
+  async (req, res) => {
+    const id = Number(req.body.id);
+    const updatedUser = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        subscribed: true,
+      },
+      include: {
+        videoPurchase: true,
+      },
+    });
 
-  const JWT = createJWT(updatedUser);
+    const JWT = createJWT(updatedUser);
 
-  return res.status(200).send({ JWT });
-});
+    return res.status(200).send({ JWT });
+  }
+);
+
+subscriptionRouter.post(
+  "/single_video",
+
+  async (req, res) => {
+    const videoId = req.body.videoId;
+    const userId = req.body.userId;
+
+    console.log({ videoId, userId });
+    const newPurchase = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        videoPurchase: {
+          create: {
+            videoId,
+          },
+        },
+      },
+      include: {
+        videoPurchase: true,
+      },
+    });
+
+    const JWT = createJWT(newPurchase);
+
+    return res.status(200).send({ JWT });
+  }
+);
