d69e94362e2048860e4743c3911c7e337e41b7e1
Alex Digrazia <alexndigrazia@hotmail.com>
2025-01-05 04:40:52 -0800

    add cancel_url for checkout form

--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -11,6 +11,7 @@ stripeRouter.post("/", async (req, res) => {
   const mode = req.body.mode;
   const path = req.body.return_url;
   const customer_email = req.body.customer_email;
+  const cancel_url = req.body.cancel_url;
   const videoId = req.body.videoId;
   const return_url =
     mode === "subscription"
@@ -30,6 +31,7 @@ stripeRouter.post("/", async (req, res) => {
     return_url,
     automatic_tax: { enabled: true },
     customer_email,
+    cancel_url,
   });
 
   res.send({ clientSecret: session.client_secret });
